90de8932a53b1447059cf77fc10f78c289eb526c
8schink <8schink@informatik.uni-hamburg.de>
2025-02-13 12:43:59 +0100

    minor changes

--- main.py
+++ main.py
@@ -5,21 +5,23 @@ class ToDoApp:
     def __init__(self, root):
         self.root = root
         self.root.title("todo list")
-
-        # Load tasks from storage
         self.tasks = load_tasks()
 
-        # Task List Frame
+        # headline
+        self.headline = tk.Label(root, text="todo list", font=("Arial", 16, "bold"))
+        self.headline.pack(pady=(10, 5))
+
+        # task list frame
         self.task_frame = tk.Frame(root)
         self.task_frame.pack(pady=10)
 
-        # Task Entry Field
+        # task entry field
         self.entry = tk.Entry(root, width=40)
         self.entry.pack(pady=5)
 
-        # Buttons
+        # buttons
         self.add_button = tk.Button(root, text="add task", command=self.add_task)
-        self.add_button.pack()
+        self.add_button.pack(pady=5)
 
         self.update_task_list()
 
